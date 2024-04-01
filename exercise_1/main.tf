3e5ab82ae8503b2cff723bd89c86d6eb147a43a7
noahunor-cxe <164400234+noahunor-cxe@users.noreply.github.com>
2024-04-01 15:36:03 -0500

    Update main.tf

--- exercise_1/main.tf
+++ exercise_1/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_s3_bucket" "tf-root-module-bucket" {
   bucket = "${var.s3_bucket_name}"
   acl = "private"
-  region = "${var.s3_bucket_region}"
+  #region = "${var.s3_bucket_region}"
   
   versioning {
     enabled = true
