6d00365cfd80b61ee172742bc3d1bdcf912908ed
noahunor-cxe <164400234+noahunor-cxe@users.noreply.github.com>
2024-04-02 12:01:10 -0500

    Update main.tf

--- exercise_2/main.tf
+++ exercise_2/main.tf
@@ -6,11 +6,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region = "${var.region}"
-}
 
 resource "aws_s3_bucket" "tf-root-module-bucket" {
   bucket = "${var.s3_bucket_name}"
