3fe3d1692a3797244fb103fecd270f084f00840e
noahunor-cxe <164400234+noahunor-cxe@users.noreply.github.com>
2024-04-02 10:14:17 -0500

    Update main.tf

--- exercise_1/main.tf
+++ exercise_1/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_s3_bucket" "tf-root-module-bucket" {
+resource "aws_s3_bucket" "noah-tf-root-module-bucket" {
   bucket = "${var.s3_bucket_name}"
   acl = "private"
   #region = "${var.s3_bucket_region}"
