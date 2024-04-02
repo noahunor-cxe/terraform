e92f561ff82ff0f7758391a2c35b0eaa0de66cfc
noahunor-cxe <164400234+noahunor-cxe@users.noreply.github.com>
2024-04-02 10:15:20 -0500

    Update variables.tf

--- exercise_1/variables.tf
+++ exercise_1/variables.tf
@@ -20,7 +20,7 @@ variable "s3_bucket_region" {
 variable "s3_bucket_name" {
   type        = string
   description = "Name of the S3 bucket to be created used to store the remote state file"
-  default     = "rc-tf-bucket-training-exercise-11"
+  default     = "rc-tf-bucket-training-exercise-111"
   }
 
 variable "tag_env" {
