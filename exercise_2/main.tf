8abc0697b32b2d3e38e2a467a30d54b6e230225c
noahunor-cxe <164400234+noahunor-cxe@users.noreply.github.com>
2024-04-02 11:44:01 -0500

    Update main.tf

--- exercise_2/main.tf
+++ exercise_2/main.tf
@@ -1,15 +1,15 @@
-provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region = "${var.region}"
-}
-
 terraform {
    backend "s3" {
    bucket = "rc-tf-bucket-training-exercise-111"  #rc-tf-remote-state-bucket
    key = "terraform.tfstate"
    region = "us-east-1"
   }
+}
+
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
 }
 
 resource "aws_s3_bucket" "tf-root-module-bucket" {
