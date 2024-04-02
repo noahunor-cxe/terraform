5b56c1dc25b19150b338d6df05179f34e8547efa
Pedro Mastelaro <phmastelaro@gmail.com>
2024-04-02 17:00:55 -0300

    Update main.tf

--- exercise_2/main.tf
+++ exercise_2/main.tf
@@ -8,7 +8,7 @@ terraform {
    backend "s3" {
    bucket = "rc-tf-bucket-training-exercise-111"  #rc-tf-remote-state-bucket
    key = "terraform.tfstate"
-   region = "us-east-2"
+   region = "us-east-1"
   }
 }
 
