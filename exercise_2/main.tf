16cf666693d06088c0ba1baf2c42f5c8a80e4a3d
noahunor-cxe <164400234+noahunor-cxe@users.noreply.github.com>
2024-04-02 10:41:07 -0500

    Update main.tf

--- exercise_2/main.tf
+++ exercise_2/main.tf
@@ -15,7 +15,7 @@ terraform {
 resource "aws_s3_bucket" "tf-root-module-bucket" {
   bucket = "${var.s3_bucket_name}"
   acl = "private"
-  region = "${var.s3_bucket_region}"
+  #region = "${var.s3_bucket_region}"
   
   versioning {
     enabled = true
